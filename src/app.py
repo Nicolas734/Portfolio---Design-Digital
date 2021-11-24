7866aa0e8a099298bcce86e2dddfda4e2a16df5b
Nicolas734 <88324308+Nicolas734@users.noreply.github.com>
2021-11-24 16:14:34 -0300

    adição do jinja e app.py

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from flask import app, Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('Index.html')
+
+@app.route('/Projetos')
+def projetos():
+    return render_template('Projetos.html')
+
+@app.route('/Contatos')
+def contatos():
+    return render_template('Contatos.html')
+
+if __name__=="__main__":
+    app.run(debug=True,port=8000)
